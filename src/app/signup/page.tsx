e94ea5a78ff82c123cd7a9ccfded95c07d4520df
diego-venegas <diego.venegas.duran@gmail.com>
2023-12-01 15:22:52 -0300

    Agregada API de signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,65 @@
 import "./signup.css";
+import React, {useState} from "react";
 
 export default function SignUp (){
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+    
+        const user = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, username, password}),
+        });
+      };
+
     return (
     <main className="flex min-h-screen flex-col" style={{alignItems:"center", justifyContent:"center"}}>
         <div style={{display: "flex", flexDirection:"column", marginBottom: "20px" , width: "100px", height: "100px", backgroundColor: "red"}}>placeholder</div>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit} method="POST">
             <label htmlFor="email" className="labels">Correo electrónico</label>
-            <input className="textfields" type="email" id="email" name="email" required/>
+            <input className="textfields" 
+                   type="email" 
+                   id="email" 
+                   name="email" 
+                   value={email} 
+                   onChange={(e) => setEmail(e.target.value)} 
+                   required
+            />
 
             <label htmlFor="username" className="labels">Nombre de usuario</label>
-            <input className="textfields" type="text" id="username" name="username" required/>
+            <input className="textfields" 
+                   type="text" 
+                   id="username" 
+                   name="username" 
+                   value={username} 
+                   onChange={(e) => setUsername(e.target.value)} 
+                   required
+            />
 
             <label htmlFor="password" className="labels">Contraseña</label>
-            <input className="textfields" type="password" id="password" name="password" minLength={8} required/>
+            <input className="textfields" 
+                   type="password" 
+                   id="password" 
+                   name="password" 
+                   minLength={8} 
+                   value={password} 
+                   onChange={(e) => setPassword(e.target.value)} 
+                   required
+            />
 
             <label htmlFor="repeat-password" className="labels">Repetir contraseña</label>
-            <input className="textfields" type="password" id="repeat-password" name="repeat-password" minLength={8} required/>
+            <input className="textfields" 
+                   type="password" 
+                   id="repeat-password" 
+                   name="repeat-password"  
+                   required
+            />
 
             <input className="submit-button" type="submit" id="submit-signup" value="Registrarse"></input>
         </form>
